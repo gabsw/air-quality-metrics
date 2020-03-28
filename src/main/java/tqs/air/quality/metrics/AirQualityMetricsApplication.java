7a94a092d75a30cbcadfeb4b35ec12f7d058eb59
gabsw <sw.gabrielasantos@gmail.com>
2020-03-28 09:49:11 +0000

    Initial commit

---
+++ src/main/java/tqs/air/quality/metrics/AirQualityMetricsApplication.java
@@ -0,0 +1,13 @@
+package tqs.air.quality.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tqs/air/quality/metrics/AirQualityMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.air.quality.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
